77896f7a3a74c474c508a991512428ebf1b6ad49
alvega-g <alvarg@gmail.com>
2024-02-06 17:51:34 +0100

    monitor

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 20:38:21 by alvega-g          #+#    #+#             */
-/*   Updated: 2024/02/06 17:35:09 by alvega-g         ###   ########.fr       */
+/*   Updated: 2024/02/06 17:51:28 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ static int	ft_threads(t_data *data, int n_of_philos)
 
 	i = -1;
 	while (++i < n_of_philos)
-		if (pthread_create(&data->philo[i].thread, NULL, &ft_routine, &data->philo[i]))
+		if (pthread_create(&data->philo[i].thread, NULL, &ft_routine, &data->philo[i])) // One more for monitoring
 			return (1);
 	i = -1;
 	while (++i < n_of_philos)
